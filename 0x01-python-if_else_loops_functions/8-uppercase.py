1bb9a8dc27f6c5bcaeb181776fd12cacd7794484
BassantKhaled259 <bossyomran262@gmail.com>
2023-10-05 01:04:37 -0700

    new file & changes

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+def islower(c):
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
 def uppercase(str):
       for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
